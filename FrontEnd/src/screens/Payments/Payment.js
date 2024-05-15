f0930569f35dd7aa4bc9ab5559e0f148f3435d25
Uksman <emmanuel781@gmail.com>
2024-05-15 19:04:20 +0100

    Payment screen in process

--- FrontEnd/src/screens/Payments/Payment.js
+++ FrontEnd/src/screens/Payments/Payment.js
@@ -1,20 +1,42 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+       
+
 
 const Payment = () => {
   return (
-      <SafeAreaView className="mt-10">
-          <View>
-          </View>
-      <View>
-        <Text className="">Choose Payment Method</Text>
-     </View>
-          <View>
-              <TouchableOpacity>
-                  <Text>Pay Now</Text>
-              </TouchableOpacity>
-          </View>
+    <SafeAreaView className="mt-10 bg-white flex-1">
+      <View className="mb-10">
+        <MaterialCommunityIcons name="chevron-left" color="#000" size={25} />
+      </View>
+      <View className="mt-8 ml-5">
+        <Text className="text-2xl font-bold whitespace-nowrap">
+          Choose Payment Method
+        </Text>
+      </View>
+      <View className=" mt-24 mx-12">
+        <TouchableOpacity className="mb-2 p-4 bg-[#F2F2F2] rounded-xl flex-row justify-between">
+          <Text>Debit/Credit card</Text>
+          <MaterialCommunityIcons
+            name="credit-card-outline"
+            color="#000"
+            size={25}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity className="mb-2 p-4 bg-[#F2F2F2] rounded-xl flex-row justify-between">
+          <Text>Internet Banking</Text>
+          <MaterialCommunityIcons name="bank" color="#000" size={25} />
+        </TouchableOpacity>
+        <TouchableOpacity className="mb-2 p-4 bg-[#F2F2F2] rounded-xl flex-row justify-between">
+          <Text>PayPAl</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="mb-2 p-4 bg-[#F2F2F2] rounded-xl items-center">
+          <Text className="text-center">+ Add another option</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
